c513721034bab9915bd6e33448e6e3951b13a22f
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2019-02-16 04:30:38 +0600

    Re-Configure - Phase 1
    [skip ci]

--- release.sh
+++ release.sh
@@ -7,34 +7,29 @@ git config --global color.ui true
 baseAddr="https://releases.linaro.org/components/toolchain/binaries"
 branches="latest-4 latest-5 latest-6 latest-7"
 
-mkdir ../temp || echo -e "../temp folder creation error"
+mkdir WORK && cd WORK
 
 for branch in $branches; do
-    echo -en "Current branch is --" && echo $branch
-    echo $branch >> ../temp/branch.txt
-    tcBranch=$(cat ../temp/branch.txt)
-    echo -en "ToolChain branch is --" && echo $tcBranch
-    git checkout $tcBranch
+    echo -en "Current branch is -- " && echo $branch
+    # build-1, no-check
     echo -e "Getting new file name from release server"
-    curl -s "$baseAddr/$tcBranch/aarch64-linux-gnu/" | grep "x86_64_aarch64-linux-gnu.tar.xz" | sort | cut -d , -f3 | tail -n 2 | head -n 1 | awk '{print substr($1,2)}' | sed 's/....$//g' >> ../temp/latest-file-name.txt
-    fileName=$(cat ../temp/latest-file-name.txt)
+    curl -s "$baseAddr/$branch/aarch64-linux-gnu/" | grep "x86_64_aarch64-linux-gnu.tar.xz" | sort | cut -d , -f3 | tail -n 2 | head -n 1 | awk '{print substr($1,2)}' | sed 's/....$//g' >> $branch-archive-name.txt
+    fileName=$(cat $branch-archive-name.txt)
     echo -en "New filename is -- " && echo $fileName
-    fileAddr=$baseAddr/$tcBranch/aarch64-linux-gnu/$fileName
+    fileAddr=$baseAddr/$branch/aarch64-linux-gnu/$fileName
     echo -en "Full URL is -- " && echo $fileAddr
-    wget -q --show-progress -P ../temp/ $fileAddr
-    ls -la ../temp/ || echo "File download error"
-    md5sum ../temp/$fileName >> ../temp/latest-archive.md5
-    if cmp -s "../temp/latest-archive.md5" "release-archive.md5"; then
-        echo "Files are Identical. No need to update"
-    else
-        cd ..
-        rm -rf linaro-toolchain-latest/*
-        tar -xJvf ../temp/$fileName --directory linaro-toolchain-latest/
-        md5sum ../temp/$fileName >> linaro-toolchain-latest/release-archive.md5
-        cd linaro-toolchain-latest/
-        git add -A .
-        git commit -m "Update Release at $(date +%Y%m%d-%H%M)"
-        ##git push origin ${TC_Branch}
-        git push -q https://$GitOAUTHToken@github.com/rokibhasansagar/linaro-toolchain-latest.git HEAD:$branch
-    fi
+    # get archive
+    wget -q --show-progress --progress=bar:force 2>&1 $fileAddr
+    md5sum $fileName | awk '{print $1}' >> $branch-archive.md5
+    cd ..
+    mkdir linaro-toolchain-latest && cd linaro-toolchain-latest
+    git init && git checkout $branch
+    tar -xJvf ../WORK/$fileName -C .
+    cp -a ../WORK/$branch-archive.md5 .
+    git add -A .
+    git commit -m "Release $branch Linaro Toolchain x86_64 Binaries at $(date +%Y%m%d-%H%M)"
+    git remote add origin https://github.com/rokibhasansagar/linaro-toolchain-latest.git
+    git push -q https://$GitOAUTHToken@github.com/rokibhasansagar/linaro-toolchain-latest.git HEAD:$branch
+    cd ..
+    rm -rf linaro-toolchain-latest
 done
